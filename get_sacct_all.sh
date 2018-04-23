e57834d807e829f36f7e502883e59c8cb1449d82
christophernhill <cnh@mit.edu>
2018-04-23 13:22:51 -0400

    Update get_sacct_all.sh

--- get_sacct_all.sh
+++ get_sacct_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -f
 headers=`sacct -e | awk '{for(n=0;n<NF;++n){printf("%s,",$(n+1))}}END{printf("\n")}' | sed s'/,$//'`
-sacct -P -o $headers -j 8781354 
+sacct -S 2017-04-23T00:00:00 -E 2017-04-23T23:59:59 -P -o $headers
